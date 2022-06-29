d9afff5154a6fd8cf68cc24009844c006e17c625
chris <c56984200123@gmail.com>
2022-06-29 23:53:23 +0800

    udpate my profile content

--- components/MyProfile.vue
+++ components/MyProfile.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="profile">
-    <img src="~/assets/avatar.png" class="avatar" alt="My Avatar" />
-    <h1>Chris Wang</h1>
+  <div>
+    <div class="profile">
+      <img src="~/assets/avatar.png" class="avatar" alt="My Avatar" />
+      <h1>Chris Wang</h1>
+      <p>Keep Learning, and Keep Coding.</p>
+    </div>
+    <div class="introduction">
+      <p>長期耕耘且涉略前端的後端工程師 • 期許自己每天多懂一點新知識 • 喜歡看劇、種植、小九兒</p>
+    </div>
   </div>
 </template>
 
@@ -12,6 +18,17 @@ export default {
 </script>
 
 <style scoped>
+.profile {
+  background-color: #f9f2e9;
+  padding: 20px;
+}
+
+.introduction {
+  background-color: #f9f2e9;
+  padding: 20px;
+  margin-top: 20px;
+}
+
 .avatar {
   border-radius: 50%;
   width: 150px;
