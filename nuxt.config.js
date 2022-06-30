8f364e09f06ba9fa561d6de8868debe9982418f3
chris <c56984200123@gmail.com>
2022-06-30 23:59:44 +0800

    update website title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: 'Blog - Chris',
     htmlAttrs: {
       lang: 'en',
     },
