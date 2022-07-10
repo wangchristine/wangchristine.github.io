569499a39ee9df9981f9c44e4e82e66468e4e35e
chris <c56984200123@gmail.com>
2022-07-10 17:09:56 +0800

    add popular posts block

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="side-block">
     <MyProfile />
+    <div class="popular">
+      <div class="title">熱門文章 📌</div>
+      <div class="list">
+        <NuxtLink
+          v-for="article in articlesByPopular"
+          :key="article.title"
+          class="article"
+          :to="{
+            name: 'article-category-slug',
+            params: { category: article.category, slug: article.slug },
+          }"
+        >
+          {{ article.title }}
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +27,13 @@ export default {
   components: {
     MyProfile,
   },
+  computed: {
+    articlesByPopular() {
+      return this.$store.getters.getAllArticles.filter(
+        (article) => article.isPopular === true
+      );
+    },
+  },
 };
 </script>
 
@@ -19,5 +42,27 @@ export default {
   width: 400px;
   text-align: center;
   margin: 0 0 40px 40px;
+}
+
+.popular {
+  background-color: #f9f2e9;
+  padding: 20px;
+  margin-top: 20px;
+}
+.popular .title {
+  font-size: 18px;
+  border-bottom: 1px solid #9b9b9b;
+  padding: 5px;
+}
+
+.popular .list {
+  margin-top: 10px;
+}
+
+.popular .article {
+  display: block;
+  text-decoration: none;
+  padding: 5px;
+  color: #9f3448;
 }
 </style>
--- content/coding/javascript-regular-expression.md
+++ content/coding/javascript-regular-expression.md
@@ -2,6 +2,7 @@
 category: coding
 title: Javascript 正則表達式 Regular Expression 詳細範例用法
 description: '每次要用正則表達式 Regular Expression 的時候，很容易忘記它的規則，因為英文我看的比較習慣且解釋的很精確'
+isPopular: true
 createdAt: '2022-07-02'
 updatedAt: '2022-07-02'
 ---
