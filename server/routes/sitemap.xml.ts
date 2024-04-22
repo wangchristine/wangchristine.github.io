9192e997dee6ea3ef565ff5424115197fb28265a
chris <c56984200123@gmail.com>
2024-04-23 00:17:55 +0800

    add sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   // Fetch all documents
   const docs = await serverQueryContent(event).find()
   const sitemap = new SitemapStream({
-    hostname: 'http://localhost:3000'
+    hostname: 'https://wangchristine.github.io'
   })
 
   for (const doc of docs) {
