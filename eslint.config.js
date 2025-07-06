18813a4c5d092964568703ca47697b948b0938e5
chris <c56984200123@gmail.com>
2025-07-06 14:53:37 +0800

    feat: udgrade packages

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import js from "@eslint/js";
+import { globalIgnores } from "eslint/config";
+import configPrettier from "eslint-config-prettier";
+import pluginImport from "eslint-plugin-import";
+import pluginPrettier from "eslint-plugin-prettier";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import { createConfigForNuxt } from "@nuxt/eslint-config";
+
+export default createConfigForNuxt([
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{js,mjs,jsx,vue}"],
+  },
+
+  globalIgnores(["**/dist/**", "**/dist-ssr/**", "**/coverage/**"]),
+
+  {
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs["flat/strongly-recommended"],
+
+  {
+    plugins: {
+      prettier: pluginPrettier,
+      import: pluginImport,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "vue/block-order": [
+        "error",
+        {
+          order: ["script", "template", "style"],
+        },
+      ],
+      "import/order": [
+        "error",
+        {
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+    },
+  },
+  configPrettier,
+]);
